af2a3fc10acc031784d8aa174edeedb15b31012a
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2022-08-10 22:37:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ app.use(express.json())
 
 app.use((req, res, next) => {
  console.log(req.path, req.method)
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+if (Context.Request.HttpMethod.Equals("OPTIONS")) {
+        Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+        Context.ApplicationInstance.CompleteRequest();
     // console.log('app.use function running real quick')
     next()
 })
