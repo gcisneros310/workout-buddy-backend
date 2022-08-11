ae35fcfb8ce9ee228644ed7c12b61ad1f0ea3f8d
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2022-08-10 22:14:06 -0700

    stuff

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
+
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     // console.log('app.use function running real quick')
     next()
 })
