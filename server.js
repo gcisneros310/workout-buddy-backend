c8f9294663b137fad6473e0688efa87546bf280e
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2022-08-10 23:09:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,19 @@ const app = express()
 //middleware
 app.use(express.json())
 
+
+var corsMiddleware = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+
+    next();
+}
+
+app.use(corsMiddleware);
+
 app.use((req, res, next) => {
  console.log(req.path, req.method)
-  res.setHeader("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     // console.log('app.use function running real quick')
     next()
 })
